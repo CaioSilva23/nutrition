2f4c591a0090fd46916e67d1c90de145e0d9f2c4
caio silva <caio_ceac23@hotmail.com>
2023-06-12 17:54:23 -0300

    update

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,7 @@ app_name = 'recipe'
 
 urlpatterns = [
     path('', views.recipe_list, name='list'),
-    path('recipe/<int:pk>/', views.recipe_detail, name='detail')
+    path('recipe/<int:pk>/', views.recipe_detail, name='detail'),
+
+    path('category/<int:pk>/', views.category_list, name='category')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from utils.factory import make_recipe
+from .models import Recipe
 
 
+# 'recipes': [make_recipe() for i in range(0, 10)]
 def recipe_list(request):
-    return render(request, 'recipes/recipe_list.html')
-
+    ctx = {
+        'recipes': Recipe.objects.filter(is_published=True)
+    }
+    return render(request, 'recipes/recipe_list.html', ctx)
 
 
 def recipe_detail(request, pk):
-    return render(request, 'recipes/recipe_detail.html')
+    ctx = {
+        'recipe': get_object_or_404(Recipe, id=pk, is_published=True),
+        'detail': True
+    }
+    return render(request, 'recipes/recipe_detail.html', ctx)
+
+
+def category_list(request, pk):
+    ctx = {
+        'recipes': get_list_or_404(Recipe, category__id=pk, is_published=True)
+    }
+
+    return render(request, 'recipes/recipe_list.html', ctx)
