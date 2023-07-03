bbbe77b769f9bae21c88cd123aed6a8af4512a8c
caio silva <caio_ceac23@hotmail.com>
2023-07-03 05:55:09 -0300

    tag app

---
+++ tag/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    # campos para relação generica
+ 
